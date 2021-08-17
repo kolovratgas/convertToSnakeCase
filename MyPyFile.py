3ed50f4ceff9b5c04ebd87b78e2e6311d4bdecf7
Roman <80954194+kolovratgas@users.noreply.github.com>
2021-08-17 16:31:28 +0400

    Add files via upload

---
+++ MyPyFile.py
@@ -0,0 +1,23 @@
+# объявление функции
+def convert_to_python_case(text):
+    newShit = list()
+    zag = list()
+    zagText = 'text'
+    lowLine = '_'
+    for i in range(len(text)):
+        if i == 0:
+            newShit.append(text[i].lower())
+        elif text[i].isupper():
+            newShit.insert(i, '_'+text[i].lower())
+        else:
+            newShit.append(text[i].lower())
+
+    newShit = ''.join(newShit)
+    return newShit
+
+
+# считываем данные
+txt = input()
+
+# вызываем функцию
+print(convert_to_python_case(txt))
